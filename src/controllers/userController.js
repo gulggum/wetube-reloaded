bbac3e38358bf7f77e79b83543d84275591e0292
gulggum <163222604+gulggum@users.noreply.github.com>
2024-06-13 12:24:26 +0900

    backend_80%

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 import multer from "multer";
@@ -201,11 +202,14 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
+  console.log(user);
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found" });
   }
+  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
-    pageTitle: user.name + "'s Profile",
+    pageTitle: user.name,
+    user,
   });
 };
